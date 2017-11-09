0329f7a9c4cc7d51b0dc1621d912e630543f9c77
Trevor Sargent <tbird1710@gmail.com>
2017-11-08 19:02:51 -0800

    pick-up-where-you-were in animation

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const { Observable, Scheduler } = require('rxjs/Rx')
+const { Observable, Scheduler, Subject } = require('rxjs/Rx')
 
 const levelInput = document.getElementById('input-level')
 const outputLevel = document.getElementById('output-level')
 const outputText = document.getElementById('output-text')
-let output = 0;
 
-// let channel
+let state$ = new Subject
+state$.startWith(0)
+
+// maps distance and time in decimal (from duration()) 
+const distance = (d) => (t) => d * t
+
+//emits a stream from 0 to 1 over the milliseconds specified.
+const duration = (ms) =>
+	msEllapsed()
+		.map(msel => msel / ms)
+		.takeWhile(x => x <= 1.0)
+
+const msEllapsed = (scheduler = Scheduler.animationFrame) =>
+	Observable.defer(() => {
+		const start = scheduler.now()
+		return Observable.interval(0, scheduler).map(() => scheduler.now() - start)
+	})
 
 const sanitizeLevelInput = x => {
 	if (x > 100) return 100
@@ -14,30 +29,14 @@ const sanitizeLevelInput = x => {
 	return x
 }
 
-const msEllapsed = (scheduler = Scheduler.animationFrame) =>
-	Observable.defer(() => {
-		const start = scheduler.now()
-		return Observable.interval(0, scheduler).map(() => scheduler.now() - start)
-	})
+const commands$ = Observable.fromEvent(levelInput, 'change')
+	.map(x => x.target.value)
+	.map(sanitizeLevelInput)
 
-const prevAndCurrent = (initialValue) => ($source) =>
-	$source.startWith(initialValue)
-		.map(x => {
-			return [output, x]
-		})
 
-//emits a stream from 0 to 1 over the milliseconds specified.
-const duration = (ms) =>
-	msEllapsed()
-		.map(msel => msel / ms)
-		.takeWhile(x => x <= 1.0)
-
-// maps distance and time in decimal (from duration()) 
-const distance = (d) => (t) => d * t
-
-const tween = (ms) => ($source) =>
-	$source
-		.let(prevAndCurrent(0))
+// takes a stream of values to animate to and emits tweened values
+const tween = (ms, state$) => (source$) =>
+	source$.withLatestFrom(state$, (src, ste) => [ste, src])
 		.switchMap(([p, n]) => {
 			const next = Number.parseInt(n)
 			const prev = Number.parseInt(p)
@@ -47,16 +46,12 @@ const tween = (ms) => ($source) =>
 		}
 		)
 
-const channel = Observable.fromEvent(levelInput, 'change')
-	.map(x => x.target.value)
-	.map(sanitizeLevelInput)
-	.let(tween(5000))
+const animator = commands$
+	.let(tween(5000, state$.startWith(0)))
 	.do(x => {
 		outputLevel.style.width = x * 10
 		outputText.innerHTML = x
-		output = x
+		state$.next(x)
 	})
 
-
-
-channel.subscribe()
+animator.subscribe()
