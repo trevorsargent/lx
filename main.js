8f3257fd87b37113b6ed4d0366d997e061ea5e08
Trevor Sargent <tbird1710@gmail.com>
2018-05-13 15:14:15 -0700

    updating channel generation

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const _ = require('highland')
 const T = require('@tweenjs/tween.js')
 
+const NUM_CHANNELS = 10;
+
+const setupChannels = () => {
+  let s = {}
+  for (let i = 1; i <= NUM_CHANNELS; i++) {
+    s[i] = 0;
+  }
+  return s
+}
+
+let channels = setupChannels()
+
 const makeSetOutputLevel = (channel) => {
   return setOutputLevel = (level) => {
     const bar = document.getElementById('output-level-' + channel)
@@ -13,33 +25,6 @@ function animate (time) {
   T.update(time);
 }
 requestAnimationFrame(animate);
-
-const form1 = document.getElementById('form-1')
-const form2 = document.getElementById('form-2')
-
-const levelField1 = document.getElementById('input-level-1')
-const levelField2 = document.getElementById('input-level-2')
-const timeField1 = document.getElementById('input-time-1')
-const timeField2 = document.getElementById('input-time-2')
-
-form1.onsubmit = (e) => {
-  e.preventDefault()
-  const target = Number.parseFloat(levelField1.value)
-  const time = Number.parseFloat(timeField1.value)
-  fadeValue(1, target, time, makeSetOutputLevel)
-}
-
-form2.onsubmit = (e) => {
-  e.preventDefault()
-  const target = Number.parseFloat(levelField2.value)
-  const time = Number.parseFloat(timeField2.value)
-  fadeValue(2, target, time, makeSetOutputLevel)
-}
-
-let channels = {
-  1: 0,
-  2: 0
-}
 
 const fadeValue = (channel, target, time, outputFunc) => {
   new T.Tween(channels)
