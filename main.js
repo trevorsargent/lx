bbf1ac1e6d7b69622c94d579bd05af796f18bad0
Trevor Sargent <tbird1710@gmail.com>
2018-05-13 14:28:32 -0700

    multi-track drifting with tween.js

--- main.js
+++ main.js
@@ -1,39 +1,52 @@
 const _ = require('highland')
+const T = require('@tweenjs/tween.js')
 
-const bar = document.getElementById('output-level')
-
-const setOutputLevel = (level) => {
-  bar.style.width = level + "%"
-}
-
-const fadeValue = (start, target, time, outputFunc) => {
-  let startTime = null
-  let distance = target - start
-  function step (timestamp) {
-    if (!startTime) startTime = timestamp;
-    var progress = timestamp - startTime;
-    let now = start + (distance * progress / (time * 1000))
-    outputFunc(now)
-    if (progress < time * 1000) {
-      window.requestAnimationFrame(step);
-    } else {
-      outputFunc(target)
-    }
+const makeSetOutputLevel = (channel) => {
+  return setOutputLevel = (level) => {
+    console.log(channel, level)
+    const bar = document.getElementById('output-level-' + channel)
+    bar.style.width = level + "%"
   }
-  window.requestAnimationFrame(step); //starts the animation
 }
 
-const form = document.getElementById('form')
-const levelField = document.getElementById('input-level')
-const timeField = document.getElementById('input-time')
+function animate (time) {
+  requestAnimationFrame(animate);
+  T.update(time);
+}
+requestAnimationFrame(animate);
 
-let currentValue = 0
+const form1 = document.getElementById('form-1')
+const form2 = document.getElementById('form-2')
 
-form.onsubmit = (e) => {
+const levelField1 = document.getElementById('input-level-1')
+const levelField2 = document.getElementById('input-level-2')
+const timeField1 = document.getElementById('input-time-1')
+const timeField2 = document.getElementById('input-time-2')
+
+form1.onsubmit = (e) => {
   e.preventDefault()
-  const target = Number.parseFloat(levelField.value)
-  const time = Number.parseFloat(timeField.value)
-  fadeValue(currentValue, target, time, setOutputLevel)
-  currentValue = target
+  const target = Number.parseFloat(levelField1.value)
+  const time = Number.parseFloat(timeField1.value)
+  fadeValue(1, target, time, makeSetOutputLevel)
 }
 
+form2.onsubmit = (e) => {
+  e.preventDefault()
+  const target = Number.parseFloat(levelField2.value)
+  const time = Number.parseFloat(timeField2.value)
+  fadeValue(2, target, time, makeSetOutputLevel)
+}
+
+let channels = {
+  1: 0,
+  2: 0
+}
+
+const fadeValue = (channel, target, time, outputFunc) => {
+  new T.Tween(channels)
+    .to({ [channel]: target }, time * 1000)
+    .onUpdate((() => {
+      outputFunc(channel)(channels[channel])
+    }))
+    .start();
+}
