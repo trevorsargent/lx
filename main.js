85ea1d88357c08744280640766daadeaab05b086
Trevor Sargent <tbird1710@gmail.com>
2018-05-13 13:08:17 -0700

    proof of concept

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 const _ = require('highland')
 
 const bar = document.getElementById('output-level')
+
 const setOutputLevel = (level) => {
   bar.style.width = level + "%"
 }
 
-
-const fadeValue = (start, target, time) => {
+const fadeValue = (start, target, time, outputFunc) => {
   let startTime = null
   let distance = target - start
-
   function step (timestamp) {
     if (!startTime) startTime = timestamp;
     var progress = timestamp - startTime;
     let now = start + (distance * progress / (time * 1000))
-    setOutputLevel(now)
+    outputFunc(now)
     if (progress < time * 1000) {
       window.requestAnimationFrame(step);
     } else {
-      setOutputLevel(target)
+      outputFunc(target)
     }
   }
   window.requestAnimationFrame(step); //starts the animation
@@ -34,7 +33,7 @@ form.onsubmit = (e) => {
   e.preventDefault()
   const target = Number.parseFloat(levelField.value)
   const time = Number.parseFloat(timeField.value)
-  fadeValue(currentValue, target, time)
+  fadeValue(currentValue, target, time, setOutputLevel)
   currentValue = target
-  console.log(currentValue)
 }
+
